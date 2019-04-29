a91904db08af101d33a0a7d52d08f2ca06124680
pprakhar75 <36911272+pprakhar75@users.noreply.github.com>
2019-04-29 20:27:06 +0530

    Contains the tables and database connectivity

---
+++ model.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+engine = create_engine('mysql+pymysql://root:12345@localhost/studentDatabase')
+
+engine.connect()
+Session = sessionmaker()
+Session.configure(bind=engine)
+
+
+class Student(Base):
+    __tablename__ = 'student'
+    roll_number = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(20))
+    age = Column((Integer))
+    gender = Column(String(20))
+
+
+Base.metadata.create_all(engine)
+session = Session()
